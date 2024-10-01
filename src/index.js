c3ee2ac749b27b60a4ad10211e0ac0df61af7cf4
Isabeleon21 <isabella.eon@gmail.com>
2024-10-01 14:19:30 +0100

    Added SheCodes API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayRecepie(response) {
+  console.log("poem generated");
   new Typewriter("#recepie", {
-    strings: "Esta es la receta",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
+    cursor: "",
   });
 }
+
+function generateRecepie(event) {
+  event.preventDefault();
+  let ingredientInput = document.querySelector("#ingredient-input");
+  let key = `01btdd9b12oabf9004aa243803e97c7e`;
+  let context = `You are a chef and you need to create a recepie from one ingredient. Format the recepie in basic HTML with all ingredients in a list and then bullet poiint step by step instructions. Go directly to the recepie exclude any tags or additons in the beggining `;
+  let prompt = `User instruction: Generate a full recepie with list of ingredient including ${ingredientInput.value} and how to prepare it`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  console.log("api thinking");
+  axios.get(apiUrl).then(displayRecepie);
+}
 let recepieFormElement = document.querySelector("#recepie-form");
-recepieFormElement.addEventListener("submit", generatePoem);
+recepieFormElement.addEventListener("submit", generateRecepie);
