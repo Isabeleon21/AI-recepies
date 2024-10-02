68388ffa08bd0cab91d6b44553a2b55a6f6a4cb3
Isabeleon21 <isabella.eon@gmail.com>
2024-10-02 15:47:18 +0100

    UX improvement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function displayRecepie(response) {
-  console.log("poem generated");
+  console.log("Recepie generated");
   new Typewriter("#recepie", {
     strings: response.data.answer,
     autoStart: true,
@@ -15,6 +15,10 @@ function generateRecepie(event) {
   let context = `You are a chef and you need to create a recepie from one ingredient. Format the recepie in basic HTML with all ingredients in a list and then bullet poiint step by step instructions. Go directly to the recepie exclude any tags or additons in the beggining `;
   let prompt = `User instruction: Generate a full recepie with list of ingredient including ${ingredientInput.value} and how to prepare it`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  let recepieElement = document.querySelector("#recepie");
+  recepieElement.classList.remove("hidden");
+  recepieElement.innerHTML = "Generating Recepie";
 
   console.log("api thinking");
   axios.get(apiUrl).then(displayRecepie);
